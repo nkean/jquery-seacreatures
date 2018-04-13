c640a326e1518bbc4e4fdee3bd0f48694c88d006
nkean <nathankean2@gmail.com>
2018-04-13 11:43:55 -0500

    Button and counter working

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 console.log('Javascript works!');
 
+let clickCounter = 0;
+
 $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery works!');
+    $('#countUpButton').click( function(){
+        clickCounter += 1;
+        $('#clickCount').text(clickCounter);
+    });
 }
