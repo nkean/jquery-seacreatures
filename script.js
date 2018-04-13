7cc79f5b14d73d21156ee716fbc178b33a6d4261
nkean <nathankean2@gmail.com>
2018-04-13 15:11:28 -0500

    This will allow user to add new creature

--- script.js
+++ script.js
@@ -7,10 +7,36 @@ $(document).ready(onReady);
 function onReady() {
     console.log('jQuery works!');
     $('#countUpButton').click( clickHandler );
+    updateDomCount();
 }
 
 function clickHandler() {
     console.log('countUpButton was clicked!');
-    clickCounter += 1;
-    $('#clickCount').text(clickCounter);
+    clickCounter++;
+    console.log(clickCounter);
+    updateDomCount();
+    addNewCreature();
+    clearInputs();
+}
+
+function updateDomCount () {
+    $('#clickCount').text('Sea Creature Count: ' + clickCounter);
+}
+
+function addNewCreature () {
+    // output to table
+
+    let creatureNameData = '<td>' + $('#newCreatureName').val() + '</td>';
+    let creatureSizeData = '<td>' + $('#newCreatureSize').val() + ' ft</td>';
+    let creatureTypeData = '<td>' + $('#newCreatureType').val() + '</td>';
+    let creatureSpeedData = '<td>' + $('#newCreatureSpeed').val() + ' mph</td>';
+
+    $('#creatureList').prepend('<tr>' + creatureNameData + creatureSizeData + creatureTypeData + creatureSpeedData + '</tr>')
+}
+
+function clearInputs () {
+    $('#newCreatureName').clear();
+    $('#newCreatureSize').clear();
+    $('#newCreatureType').clear();
+    $('#newCreatureSpeed').clear();
 }
